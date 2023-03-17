47ff40c9e7313a255771b71e19a445aaa52d0644
Lucas Alponti <122104166+lucasbalponti@users.noreply.github.com>
2023-03-17 20:31:05 -0300

    Atualização comentários

--- Ciência de dados/Manipulação e importação - base top 25 ações.py
+++ Ciência de dados/Manipulação e importação - base top 25 ações.py
@@ -4,19 +4,24 @@ from datetime import date
 from datetime import datetime
 import urllib.parse
 
+# Importando a base de dados das 25 empresas com maior valuation via API
 base_25top = pd.DataFrame(requests.get('https://brapi.dev/api/quote/list?sortBy=market_cap_basic&sortOrder=desc&limit=25').json()['stocks'])
 
+# Mudando os pontos por virgulas, para que os dados fiquemno formato adequado
 base_25top.loc[:, 'close'] = [str(base_25top['close'][i]).replace('.',',') for i in range(len(base_25top))]
 base_25top.loc[:, 'change'] = [str(base_25top['change'][i]).replace('.',',') for i in range(len(base_25top))]
 base_25top.loc[:, 'market_cap'] = [str(base_25top['market_cap'][i]).replace('.',',') for i in range(len(base_25top))]
 
+# Criando uma string com o nome de todas as ações importadas para importar o desempenho histórico das mesmas
 str_stocks = base_25top['stock'].values[0]
 
 for i in range(len(base_25top) - 1):
     str_stocks = str_stocks + ',' + base_25top['stock'].values[i+1]
 
+# Importando a base de dados do desempenho histórico
 json_resultados = requests.get('https://brapi.dev/api/quote/'+ urllib.parse.quote_plus(str_stocks) +'?range=5y&interval=1d&fundamental=true').json()
 
+# Montando a base de dados com o desempenho histórico das ações
 base_historico = pd.DataFrame()
 
 for i in range(len(json_resultados['results'])):
